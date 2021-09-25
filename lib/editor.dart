9f43fc5f7f380b8af451595194bde2794b813186
Yoshihiro Tanaka <contact@cordea.jp>
2021-09-25 13:33:50 +0900

    apply text style

--- lib/editor.dart
+++ lib/editor.dart
@@ -1,6 +1,55 @@
 import 'package:flutter/material.dart';
 
-class MarkdownTextEditingController extends TextEditingController {}
+class MarkdownTextEditingController extends TextEditingController {
+  final detector = MarkdownElementDetector();
+
+  @override
+  TextSpan buildTextSpan({
+    required BuildContext context,
+    TextStyle? style,
+    required bool withComposing,
+  }) {
+    final text = value;
+    final spans = text.text.split('\n').map((e) {
+      final line = '$e\n';
+      switch (detector.detect(e)) {
+        case MarkdownElement.h1:
+          return TextSpan(
+            text: line,
+            style: Theme.of(context).textTheme.headline1,
+          );
+        case MarkdownElement.h2:
+          return TextSpan(
+            text: line,
+            style: Theme.of(context).textTheme.headline2,
+          );
+        case MarkdownElement.h3:
+          return TextSpan(
+            text: line,
+            style: Theme.of(context).textTheme.headline3,
+          );
+        case MarkdownElement.h4:
+          return TextSpan(
+            text: line,
+            style: Theme.of(context).textTheme.headline4,
+          );
+        case MarkdownElement.h5:
+          return TextSpan(
+            text: line,
+            style: Theme.of(context).textTheme.headline5,
+          );
+        case MarkdownElement.h6:
+          return TextSpan(
+            text: line,
+            style: Theme.of(context).textTheme.headline6,
+          );
+        case MarkdownElement.plain:
+          return TextSpan(text: line);
+      }
+    }).toList(growable: false);
+    return TextSpan(children: spans);
+  }
+}
 
 @visibleForTesting
 class MarkdownElementDetector {
